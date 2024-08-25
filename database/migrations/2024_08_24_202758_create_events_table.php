00130d96a4d5bfde6204338d90e392155f5089f2
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-08-25 14:23:15 +0200

    event model mfs

---
+++ database/migrations/2024_08_24_202758_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\event_category;
+use App\Models\event_tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(event_category::class)->constrained();
+            $table->foreignIdFor(event_tag::class)->constrained();
+            $table->foreignId('creator_id')->references('id')->on('users');
+//            $table->string('slug');
+//            $table->string('picture');
+            $table->string('title');
+            $table->string('description');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('region')->nullable();
+            $table->boolean('is_IRL');
+//            $table->integer('participant_min');
+            $table->integer('participant_max');
+            $table->timestamp('start_date');
+//            $table->timestamp('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
