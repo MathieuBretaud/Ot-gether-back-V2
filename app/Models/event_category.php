0ab559a9c338bf37d5644d9a2514f034976adf8a
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-08-25 14:24:15 +0200

    user event table pivot

--- app/Models/event_category.php
+++ app/Models/event_category.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class event_category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'image_url'
+    ];
+
+//    protected static function booted()
+//    {
+//        static::saving(function ($category) {
+//            if (empty($category->slug)) {
+//                $category->slug = Str::slug($category->name);
+//            }
+//        });
+//    }
+//
+//    public function getImageUrlAttribute($value)
+//    {
+//        return url($value);
+//    }
+
+    public function events(): HasMany
+    {
+        return $this->hasMany(Event::class);
+    }
 }
