c0289a67363babc2421cab5e81d488c84c3e08d7
MathieuBretaud <mathieu.bretaud@gmail.com>
2024-10-13 15:42:58 +0200

    slug category auto

--- app/Models/category.php
+++ app/Models/category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Str;
 
 /**
  * @mixin IdeHelpercategory
@@ -19,15 +20,15 @@ class category extends Model
         'image_url'
     ];
 
-//    protected static function booted()
-//    {
-//        static::saving(function ($category) {
-//            if (empty($category->slug)) {
-//                $category->slug = Str::slug($category->name);
-//            }
-//        });
-//    }
-//
+    protected static function booted(): void
+    {
+        static::saving(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    }
+
 //    public function getImageUrlAttribute($value)
 //    {
 //        return url($value);
